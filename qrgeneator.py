f9389c1281e96c6794e734521075bb0f62621818
jhansijhansipalaparthi-collab <jhansijhansipalaparthi@gmail.com>
2026-05-31 08:53:37 +0530

    Add files via upload

---
+++ qrgeneator.py
@@ -0,0 +1,19 @@
+import qrcode
+
+qr = qrcode.QRCode(
+    version=5,
+    box_size=5,
+    border=2
+)
+
+name = input("enter your name:")
+age = int(input("enter your age:"))
+email = input("enter your email:")
+mobile = input("enter your mobile:") # int() teesesanu, endhukante mobile 10 digits start 0 tho unte error
+data = {"Name": name, "Age": age, "Email": email, "Mobile": mobile}
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image()
+img.save("mydetails_in_qr.png")
+img.show()
